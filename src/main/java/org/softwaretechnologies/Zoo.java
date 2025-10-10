f9d0a6edb8aa36f0bc2d37c632c697302d05bf0a
dvafanasev <bumbon4eg@gmail.com>
2025-10-10 09:50:59 +0300

    ExceptionTask and ExceptionTest is done.

--- src/main/java/org/softwaretechnologies/Zoo.java
+++ src/main/java/org/softwaretechnologies/Zoo.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Zoo {
     private final List<Animal> animalList = new ArrayList<>();
@@ -33,14 +34,9 @@ public class Zoo {
      * @return Звуки животных, в алфавитном порядке имени животного.
      */
     public List<String> soundAllAnimalsSortByName() {
-        List<String> sounds = new ArrayList<>();
-
-        animalList.sort(Comparator.comparing(Animal::getName));
-
-        for (Animal animal : animalList) sounds.add(animal.sound());
-
-        return sounds;
+        return animalList.stream()
+                .sorted(Comparator.comparing(Animal::getName))
+                .map(Animal::sound)
+                .collect(Collectors.toList());
     }
-
-
 }
